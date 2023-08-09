fafb5f8eaf46246ddee26317cc582736b6f4d700
MTofayelAhmed <tofayelahmed324@gmail.com>
2023-08-09 12:32:19 +0600

    done with navbar

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,15 +1,35 @@
+import Link from "next/link";
+
 const NavLinks = [
     {
         path:'/',
         element: 'Home'
     },
-    
-]
+    {
+        path: "/about",
+        element: "About",
+    },
+    {
+        path: "/blog",
+        element: "Blog",
+    },
+    {
+        path: "/dashboard",
+        element: "Dashboard"
+    },
+];
 
 const Navbar = () => {
     return (
-        <div>
-            
+        <div className="flex items-center justify-between">
+            <h1>Next Hero</h1>
+            <ul className=" flex items-center justify-center ">
+                {NavLinks.map(({ path, element }) => (
+                    <li className=" mx-2" key={path}>
+                        <Link href={path}>{element}</Link>
+                    </li>
+                ))}
+            </ul>
         </div>
     );
 };
